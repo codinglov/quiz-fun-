4088536250fb87a6eb9960d38e5700f63fa7c4d6
codinglov <112770238+codinglov@users.noreply.github.com>
2023-02-13 20:19:00 -0600

    origin

---
+++ Main/assets/js/questions.js
@@ -0,0 +1,35 @@
+var questions = [
+    {
+      title: 'What year did America gain its independence?',
+      choices: ['1976', '1986', '1956', '1966'],
+      answer: '1976',
+    },
+    {
+      title: 'What year did Texas become an official US state?',
+      choices: ['1871', '1841', '1861', '1851'],
+      answer: '1861',
+    },
+    {
+      title: 'Whom did the alamo affect?',
+      choices: [
+        'Americans',
+        'Spinyards',
+        'Mexicans',
+        'Mexicans and Americans',
+      ],
+      answer: 'Mexicans and Americans',
+    },
+    {
+      title:
+        'How many governers has texas had?',
+      choices: ['10','50','37','48'],
+      answer: '48',
+    },
+    {
+      title:
+        'Who is the current governer of Texas?',
+      choices: ['Rick Perry','Greg Abbot','George Bush','Ted Cruz'],
+      answer: 'Greg Abbot',
+    },
+  ];
+  
